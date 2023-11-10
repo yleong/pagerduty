0770c938622dabd4e6f0928c9b93c4efd471eef0
Leong Yew Long <yewlong42@gmail.com>
2023-11-10 21:10:33 +0800

    PAGERDUTY_USER should be the 7 char userid rather than email

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ You need to set these environment variables, then run the pagerduty binary
 ```
 PAGERDUTY_KEY=xxxxxxxxxxxxxxxxxxxx
 PAGERDUTY_PORT=xxxx
-PAGERDUTY_USER=xxxxxxx
+PAGERDUTY_USER=X8Y4Z0W
 ```
+Note: The `PAGERDUTY_USER` is the user id rather than the email.
 Then, `curl localhost:$PAGERDUTY_PORT/oncalls`
--- pagerduty.go
+++ pagerduty.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"log"
 	"os"
+	"regexp"
 
 	"github.com/yleong/pagerduty/pdapi"
 	"github.com/yleong/pagerduty/server"
 )
 
 func main() {
+	user := os.Getenv("PAGERDUTY_USER")
+	emailPattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+
+	re := regexp.MustCompile(emailPattern)
+
+	if re.MatchString(user) {
+		log.Printf("PAGERDUTY_USER (%s) looks like an email. It should be the user ID instead (e.g. X8Y4Z0W which matches [A-Z0-9]{7} )\n", user)
+		return
+	} 
+
 	s := server.Server{
 		Port: os.Getenv("PAGERDUTY_PORT"),
 		PD: pdapi.PagerDuty{
 			Key:  os.Getenv("PAGERDUTY_KEY"),
-			User: os.Getenv("PAGERDUTY_USER"),
+			User: user,
 			URL:  "https://api.pagerduty.com",
 		},
 	}
